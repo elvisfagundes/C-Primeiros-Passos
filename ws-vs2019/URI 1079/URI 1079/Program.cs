256628dd2bebe667ab1426f2cb884cdf0d846351
Elvis Fagundes <elvis.f.lisboa.jr@hotmail.com>
2021-06-10 11:38:14 -0300

    Adicionado outros Projetos

---
+++ ws-vs2019/URI 1079/URI 1079/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace URI_1079
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+             
+            for (int i = 0; i < n; i++){
+                string[] vet = Console.ReadLine().Split(' ');
+                double a = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                double b = double.Parse(vet[1], CultureInfo.InvariantCulture);
+                double c = double.Parse(vet[2], CultureInfo.InvariantCulture);
+                double mediaP = (a * 2.0 + b * 3.0 + c * 5.0) / 10;
+                Console.WriteLine(mediaP.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
