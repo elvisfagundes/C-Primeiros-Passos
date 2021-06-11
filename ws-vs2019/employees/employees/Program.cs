2f457f11be1726e9d5dba27d1c39972cf405dffe
Elvis Fagundes <elvis.f.lisboa.jr@hotmail.com>
2021-06-10 21:01:50 -0300

    Projeto employees finalizado

--- ws-vs2019/employees/employees/Program.cs
+++ ws-vs2019/employees/employees/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace employees
 {
@@ -6,7 +8,46 @@ namespace employees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<employee> list = new List<employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Emplyoee #" + i + ":");
+                Console.Write("Id: ");
+                int ident = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = (Console.ReadLine());
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new employee(ident, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the employee id that will have salary increase : ");
+            int search = int.Parse(Console.ReadLine());
+
+            employee emp = list.Find(x => x.Ident == search);
+            if (emp != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach (employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
         }
     }
 }
--- ws-vs2019/employees/employees/employee.cs
+++ ws-vs2019/employees/employees/employee.cs
@@ -1,10 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace employees
 {
     class employee
     {
-    }
+        public int Ident { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+        public employee(int ident, string name, double salary)
+        {
+            Ident = ident;
+            Name = name;
+            Salary = salary;
+        }
+        public void IncreaseSalary(double percentage)
+        {
+
+            Salary += Salary * percentage / 100.0;
+            
+        }
+        public override string ToString()
+        {
+            return Ident + ", " + Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }   
+    
 }
